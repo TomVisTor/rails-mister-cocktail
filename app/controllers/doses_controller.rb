5e347974524195bf8365cc620bc7753531da83c9
Thomas Visset <thmvisset31@gmail.com>
2021-02-25 17:15:39 +0100

    Mister Cocktail

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,32 @@
+class DosesController < ApplicationController
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      flash[:error] = "You've done wrong"
+      render 'cocktails/show'
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create(name: "ice")
+mint_leaves = Ingredient.create(name: "mint leaves")
+coca = Ingredient.create(name: "Coca Cola")
+rhum = Ingredient.create!(name: "Rhum")
+vodka = Ingredient.create(name: "Vodka")
+redbull = Ingredient.create(name: "Red Bull")
+whisky = Ingredient.create(name: "Whisky")
+
+mojito = Cocktail.create!(name: "Mojito")
+vodka_redbull  = Cocktail.create(name: "Vodka Red Bull")
+whisky_coke = Cocktail.create(name: "Whisky Coca Cola")
+
+dose_test = Dose.create!(description: "Beaucoup", ingredient: rhum, cocktail: mojito)
